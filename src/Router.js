f3659e6c648a93b55b74df4d27bbb562366cd1a5
Andy Lai <anlaics2@gmail.com>
2018-03-29 15:10:45 -0700

    added SafeTrek auth button

--- src/Router.js
+++ src/Router.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Linking } from 'react-native';
 import { Scene, Router } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
 import IntroScreen from './components/IntroScreen';
 import CreateForm from './components/CreateForm';
 import PostList from './components/PostList';
 const RouterComponent = () => {
+	const url = "https://account-sandbox.safetrek.io/authorize?audience=https://api-sandbox.safetrek.io&client_id=gk1nFtbQr4pBpJD0rzAp3vaSi555sm4s&scope=openid%20phone%20offline_access&state=statecode&response_type=code&redirect_uri=http://localhost:3000/callback";
   return (
     <Router>
     	<Scene key="root" hideNavBar>
@@ -14,7 +16,12 @@ const RouterComponent = () => {
 	      		<Scene key="loginuser" component={LoginForm} title="SoMa" />
 	      	</Scene>
 	      	<Scene key="main">
-	      		<Scene key="posts" component={PostList} title="SoMa"/>
+	      		<Scene
+	      			rightTitle="Safetrek"
+	      			onRight={() => Linking.openURL(url)}
+	      			key="postlist"
+			      	component={PostList}
+			      	title="SoMa"/>
 	      	</Scene>
       	</Scene>
     </Router>
