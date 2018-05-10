e76de77ffbd2773a72401b0c7575b15e8f88b0f9
Andy <anlaics2@gmail.com>
2018-05-10 12:20:13 -0700

    initial commit

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
-import React, { Component } from 'react';
-import { View, Text } from 'react-native';
-import { createBottomTabNavigator, createStackNavigator } from 'react-navigation';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
-export default class App extends Component {
-    render() {
-        <View>
-            <Text>Home Screen</Text>
-        </View>
-    }
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
