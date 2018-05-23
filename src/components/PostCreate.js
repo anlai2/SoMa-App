6bd9e51080f71206de9a20acc01a9482bcff7613
Andy <anlaics2@gmail.com>
2018-05-22 17:39:24 -0700

    post create redesign

--- src/components/PostCreate.js
+++ src/components/PostCreate.js
@@ -1,204 +1,261 @@
 import React, { Component } from 'react';
-import { LinearGradient, Constants, ImagePicker } from 'expo';
+import { ImagePicker } from 'expo';
 import { connect } from 'react-redux';
-import { View, Text, Switch, Picker, ScrollView } from 'react-native';
-import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
-import { postUpdate, postCreate } from '../actions';
-import { Card, Input, Button } from './common';
+import { View, Text, Switch, Picker } from 'react-native';
+import { Button } from 'react-native-elements';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import { Actions } from 'react-native-router-flux';
 import * as firebase from 'firebase';
 import uuid from 'uuid';
+import { postUpdate, postCreate } from '../actions';
+import { Input } from './common';
 
 class PostCreate extends Component {
-	constructor(props) {
-		super(props);
-		const id = null;
+  constructor(props) {
+    super(props);
+    const id = null;
 
-		this.state = { id };
-	}
+    this.state = { id };
+  }
 
-	onCameraPress = async () => {
-		let result = await ImagePicker.launchCameraAsync();
+  onCameraPress = async () => {
+    const result = await ImagePicker.launchCameraAsync();
 
-		if (!result.cancelled) {
-			this.uploadImage(result.uri)
-				.then(() => {
-					alert("Success");
-					this.downloadURL()
-				})
-				.catch((error) => {
-					alert(error);
-				});
-		}
-	}
+    if (!result.cancelled) {
+      this.uploadImage(result.uri)
+        .then(() => {
+          alert('Success');
+          this.downloadURL();
+        })
+        .catch((error) => {
+          alert(error);
+        });
+    }
+  };
 
-	onChooseImagePress = async () => {
-		let result = await ImagePicker.launchImageLibraryAsync();
+  onChooseImagePress = async () => {
+    const result = await ImagePicker.launchImageLibraryAsync();
 
-		if (!result.cancelled) {
-			this.uploadImage(result.uri)
-				.then(() => {
-					alert("Success");
-					this.downloadURL()
-				})
-				.catch((error) => {
-					alert(error);
-				});
-		}
-	}
+    if (!result.cancelled) {
+      this.uploadImage(result.uri)
+        .then(() => {
+          alert('Success');
+          this.downloadURL();
+        })
+        .catch((error) => {
+          alert(error);
+        });
+    }
+  };
 
-	onUploadPress() {
-		const { imageID } = this.props;
+  onUploadPress() {
+    const { imageID } = this.props;
 
-		this.props.post({ imageID });
-	}
+    this.props.post({ imageID });
+  }
 
-	uploadImage = async (uri) => {
-		const response = await fetch(uri);
-		const blob = await response.blob();
+  onPostPress() {
+    const {
+      safeTrek,
+      postType,
+      postTitle,
+      price,
+      address,
+      imageID,
+    } = this.props;
+    const { currentUser } = firebase.auth();
 
-		this.setState({ id: uuid.v4() });
-		var ref = firebase.storage().ref().child("images/" + this.state.id);
+    this.props.postCreate({
+      user: currentUser.uid,
+      safeTrek: safeTrek || false,
+      postType: postType || 'Buy',
+      postTitle,
+      price,
+      address,
+      imageID,
+    });
+  }
 
-		this.props.postUpdate({ prop: 'imageID', value: this.state.id })
-		return ref.put(blob);
-	}
+  uploadImage = async (uri) => {
+    const response = await fetch(uri);
+    const blob = await response.blob();
 
-	downloadURL() {
-		firebase.storage().ref().child("images/" + this.state.id).getDownloadURL()
-			.then((url) => {
-				this.props.postUpdate({ prop: 'imageID', value: url })
-			})
-	}
+    this.setState({ id: uuid.v4() });
+    const ref = firebase
+      .storage()
+      .ref()
+      .child(`images/${this.state.id}`);
 
-	onPostPress() {
-		const { user, safeTrek, postType, postTitle, price, address, imageID } = this.props;
-		const { currentUser } = firebase.auth();
+    this.props.postUpdate({ prop: 'imageID', value: this.state.id });
+    return ref.put(blob);
+  };
 
-		this.props.postCreate({ user: currentUser.uid, safeTrek: safeTrek || false, postType: postType || 'Buy', postTitle, price, address, imageID });
-	}
+  downloadURL() {
+    firebase
+      .storage()
+      .ref()
+      .child(`images/${this.state.id}`)
+      .getDownloadURL()
+      .then((url) => {
+        this.props.postUpdate({ prop: 'imageID', value: url });
+      });
+  }
 
-	render() {
-		return (
-			<View style={styles.backgroundStyle}>
-				<KeyboardAwareScrollView
-					resetScrollToCoords={{ x: 0, y: 0 }}
-					contentContainerStyle={styles.container}
-					scrollEnabled={true}
-				>
-					<View style={styles.cardsectionContainer}>
-						<Text style={styles.switchTextStyle}>
-							SafeTrek Users Only
-								</Text>
-						<Switch
-							value={this.props.safeTrek}
-							onValueChange={bool => this.props.postUpdate({ prop: 'safeTrek', value: bool })}
-						/>
-					</View>
+  render() {
+    return (
+      <View style={styles.backgroundStyle}>
+        <KeyboardAwareScrollView
+          resetScrollToCoords={{ x: 0, y: 0 }}
+          contentContainerStyle={styles.container}
+          scrollEnabled
+        >
+          <View style={styles.cardSectionContainer}>
+            <Button
+              title="BUYING"
+            />
+            <Button
+              title="SELLING"
+            />
+          </View>
+          <View style={styles.cardSectionContainer}>
+            <Input
+              label="Post Title"
+              placeholder="Post Title"
+              value={this.props.postTitle}
+              onChangeText={text =>
+                this.props.postUpdate({ prop: 'postTitle', value: text })
+              }
+            />
+          </View>
 
-					<View style={styles.cardsectionContainer}>
-						<Text style={styles.pickerTextStyle}>Buy/Sell</Text>
-						<Picker
-							style={{ flex: 1 }}
-							selectedValue={this.props.postType}
-							onValueChange={value => this.props.postUpdate({ prop: 'postType', value })}
-						>
-							<Picker.Item label="Buying" value="Buying" />
-							<Picker.Item label="Selling" value="Selling" />
-						</Picker>
-					</View>
+          <View style={styles.cardSectionContainer}>
+            <Input
+              label="Price"
+              placeholder="$"
+              value={this.props.price}
+              onChangeText={text =>
+                this.props.postUpdate({ prop: 'price', value: text })
+              }
+            />
+          </View>
 
-					<View style={styles.cardsectionContainer}>
-						<Input
-							label="Post Title"
-							placeholder="Post Title"
-							value={this.props.postTitle}
-							onChangeText={text => this.props.postUpdate({ prop: 'postTitle', value: text })}
-						/>
-					</View>
+          <View style={styles.cardSectionContainer}>
+            <Input
+              label="Meeting"
+              placeholder="Address"
+              value={this.props.address}
+              onChangeText={text =>
+                this.props.postUpdate({ prop: 'address', value: text })
+              }
+            />
+          </View>
+          <View style={styles.cardSectionContainer}>
+            <Text style={styles.switchTextStyle}>SafeTrek Users Only</Text>
+            <Switch
+              value={this.props.safeTrek}
+              onValueChange={bool =>
+                this.props.postUpdate({ prop: 'safeTrek', value: bool })
+              }
+            />
+          </View>
 
-					<View style={styles.cardsectionContainer}>
-						<Input
-							label="Price"
-							placeholder="$"
-							value={this.props.price}
-							onChangeText={text => this.props.postUpdate({ prop: 'price', value: text })}
-						/>
-					</View>
+          <View style={styles.cardSectionContainer}>
+            <Text style={styles.pickerTextStyle}>Buy/Sell</Text>
+            <Picker
+              style={{ flex: 1 }}
+              selectedValue={this.props.postType}
+              onValueChange={value =>
+                this.props.postUpdate({ prop: 'postType', value })
+              }
+            >
+              <Picker.Item label="Buying" value="Buying" />
+              <Picker.Item label="Selling" value="Selling" />
+            </Picker>
+          </View>
+          <View style={styles.buttonContainer}>
+            <Button
+              onPress={this.onChooseImagePress}
+              title="Choose an Image..."
+            />
+          </View>
 
-					<View style={styles.cardsectionContainer}>
-						<Input
-							label="Meeting"
-							placeholder="Address"
-							value={this.props.address}
-							onChangeText={text => this.props.postUpdate({ prop: 'address', value: text })}
-						/>
-					</View>
+          <View style={styles.buttonContainer}>
+            <Button
+              onPress={this.onCameraPress}
+              title="Take a Picture"
+            />
+          </View>
 
-					<View style={styles.buttonContainer}>
-						<Button onPress={this.onChooseImagePress}>
-							Choose an Image...
-						</Button>
-					</View>
+          <View style={styles.buttonContainer}>
+            <Button
+              onPress={Actions.mapScreen}
+              title="Choose a Safe Meeting Point"
+            />
+          </View>
 
-					<View style={styles.buttonContainer}>
-						<Button onPress={this.onCameraPress}>
-							Take a Photo
-						</Button>
-					</View>
-
-					<View style={styles.buttonContainer}>
-						<Button onPress={Actions.mapScreen}>
-							Pick Address on Map
-								</Button>
-					</View>
-
-
-					<View style={styles.buttonContainer}>
-						<Button onPress={this.onPostPress.bind(this)}>
-							Post
-								</Button>
-					</View>
-				</KeyboardAwareScrollView>
-			</View>
-		);
-	}
+          <View style={styles.buttonContainer}>
+            <Button
+              onPress={() => this.onPostPress()}
+              title="Post"
+            />
+          </View>
+        </KeyboardAwareScrollView>
+      </View>
+    );
+  }
 }
 
 const styles = {
-	backgroundStyle: {
-		flex: 1
-	},
-	switchTextStyle: {
-		fontSize: 18,
-		paddingLeft: 20,
-		paddingTop: 5,
-		flex: 1,
-		alignItems: 'center'
-	},
-	pickerTextStyle: {
-		fontSize: 18,
-		paddingLeft: 20
-	},
-	cardsectionContainer: {
-		flex: 1,
-		borderRadius: 25,
-		backgroundColor: "#1573E5",
-		justifyContent: 'flex-start',
-		flexDirection: 'row',
-		padding: 10
-	},
-	buttonContainer: {
-		paddingBottom: 10,
-		padding: 10
-	},
+  backgroundStyle: {
+    flex: 1,
+    backgroundColor: '#FFF',
+  },
+  switchTextStyle: {
+    fontSize: 18,
+    paddingLeft: 20,
+    paddingTop: 5,
+    flex: 1,
+    alignItems: 'center',
+  },
+  pickerTextStyle: {
+    fontSize: 18,
+    paddingLeft: 20,
+  },
+  cardSectionContainer: {
+    flex: 1,
+    borderRadius: 25,
+    justifyContent: 'flex-start',
+    flexDirection: 'row',
+    padding: 10,
+  },
+  buttonContainer: {
+    paddingBottom: 10,
+    padding: 10,
+  },
 };
 
 const mapStateToProps = (state) => {
-	const { user, safeTrek, postType, postTitle, price, address, imageID } = state.postForm;
+  const {
+    user,
+    safeTrek,
+    postType,
+    postTitle,
+    price,
+    address,
+    imageID,
+  } = state.postForm;
 
-	return { user, safeTrek, postType, postTitle, price, address, imageID }
-}
+  return {
+    user,
+    safeTrek,
+    postType,
+    postTitle,
+    price,
+    address,
+    imageID,
+  };
+};
 export default connect(mapStateToProps, {
-	postUpdate, postCreate
+  postUpdate,
+  postCreate,
 })(PostCreate);
